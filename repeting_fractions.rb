eb452e015732a08d21ca1d56d8b07cdb4c0c9aa8
MosheVB <mavanberg@gmail.com>
2015-11-15 21:29:06 -0700

    update

--- repeting_fractions.rb
+++ repeting_fractions.rb
@@ -1,12 +1,14 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
 
+num_digits = 12
+
 def generate_deicmal(x, num_digits)
-	num_digits += 1
+	num_digits += 2
 	number = BigDecimal(1).div(x, num_digits)
 	number = number.to_s
 	array = number.split("")
-	array = array.slice(2,num_digits - 1)
+	array = array.slice(2, num_digits - 1)
 	return array
 end
 
@@ -25,8 +27,23 @@ def create_repition_array(array)
 	return new_array.reject{ |e| e.empty? }
 end
 
+def find_repitition(array,num_digits)
+	counts  = Hash.new 0
+	array.each_with_index do |item,index|
+		if array[index].length <= (num_digits / 2)
 
-p create_repition_array(generate_deicmal(7,20))
+		counts[item] += 1
+		end
+	end
+	# counts.each do |number|
+	# 	puts number
+	# 		end
+	return counts
+end
+
+num_digits = num_digits + 1 if num_digits % 2 != 0
+
+p find_repitition(create_repition_array(generate_deicmal(7,num_digits)), num_digits)
 
 # notes: find the largest number that occurs more than once. then do something...
 
