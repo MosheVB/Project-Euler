9dfa2812656fd5b5c49f852ceb93132d5619dcd7
MosheVB <mavanberg@gmail.com>
2015-11-16 00:52:30 -0700

    update

--- repeting_fractions.rb
+++ repeting_fractions.rb
@@ -1,14 +1,20 @@
 require 'bigdecimal'
 require 'bigdecimal/util'
 
-num_digits = 12
+num_digits = 24
 
 def generate_deicmal(x, num_digits)
 	num_digits += 2
 	number = BigDecimal(1).div(x, num_digits)
 	number = number.to_s
 	array = number.split("")
-	array = array.slice(2, num_digits - 1)
+	index_position = 0
+	array.each_with_index do |element, index|
+		if element == "E"
+			index_position = index
+		end
+	end
+	array = array.slice(2, index_position -1)
 	return array
 end
 
@@ -29,21 +35,40 @@ end
 
 def find_repitition(array,num_digits)
 	counts  = Hash.new 0
+	max = 0
+	repition_max = 1
+	array_new = []
 	array.each_with_index do |item,index|
 		if array[index].length <= (num_digits / 2)
 
 		counts[item] += 1
 		end
 	end
-	# counts.each do |number|
-	# 	puts number
-	# 		end
-	return counts
+	counts.each do |hash, key|
+		num_length = hash.length 
+		if key >= repition_max
+			repition_max = key
+			# puts repition_max
+		end
+		if num_length >= max 
+			max = num_length
+			# puts hash
+		end
+	end 
+	while array_new.length == 0
+		counts.each do |hash, key|
+		 if hash.length == max && key == repition_max
+		 	array_new << hash
+		 end
+		end
+		repition_max -= 1 unless repition_max == 1
+	end
+	puts "Repitition count of #{repition_max} with length #{max}"
+	return array_new
 end
 
 num_digits = num_digits + 1 if num_digits % 2 != 0
 
 p find_repitition(create_repition_array(generate_deicmal(7,num_digits)), num_digits)
-
 # notes: find the largest number that occurs more than once. then do something...
 
